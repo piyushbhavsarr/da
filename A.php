1ae0a157c839bbb2f6bbc7cfd9bd68b61573a171
Piyush Bhavsar <114468009+piyushbhavsarr@users.noreply.github.com>
2023-04-26 11:08:10 +0530

    Create A.php

---
+++ A.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>AJAX Example</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  </head>
+  <body>
+    <label for="name">Name:</label>
+    <input type="text" id="name">
+    <div id="response"></div>
+    <script src="ajax.js"></script>
+  </body>
+</html>
